41421704f12fa516014a930488f08ca5a897710f
“AndreGarcia <andre.m.garcia@icloud.com>
2021-03-13 11:24:01 -0300

    Update Recipe.model.js

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+   title: { type: String, required: true, unique: true  }
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
