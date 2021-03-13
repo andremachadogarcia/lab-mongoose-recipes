13c64c9eb1dc718dca35303c45b0a4fd8ca3104e
“AndreGarcia <andre.m.garcia@icloud.com>
2021-03-13 11:42:34 -0300

    Update Recipe.model.js

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -7,6 +7,13 @@ const recipeSchema = new Schema({
    ingredients: [{ type: String }],
    cuisine: { type: String, required: true },
    dishType: { type: String, enum: [ 'breakfast', 'main_course', 'soup', 'snack', 'drink', 'dessert', 'other'] },
+   image: { type: String, default: 'https://images.media-allrecipes.com/images/75131.jpg' },
+   duration: {type: Number, min: 0 },
+   creator: { type: String },
+
+},
+{
+  timestamps: true,
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
